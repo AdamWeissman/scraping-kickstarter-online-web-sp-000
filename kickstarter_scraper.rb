1a3f91d57288825ae15b918ecee1608977dc173f
Adam Weissman <adam@comedytragedyepic.com>
2019-10-10 11:04:52 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,6 +5,10 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
+  # projects: kickstarter.css("li.project.grid_4")
+  
+  
 end
  
 create_project_hash
